f876a2f59f5dc0fefa94e7fa18eed7af110456ee
guchi0 <sp9g83t7@gmail.com>
2021-05-08 05:17:40 +0900

    imagesフォルダの作成

--- main.js
+++ main.js
@@ -17,16 +17,16 @@ serchbutton.onclick = function () {
   } else if (text == "好きな食べ物は？") {
     display.textContent =
       "焼きたてのメロンパン！　外のサクサク感と中のふわっと感のギャップに萌えますね。";
-    quizImage.src = "01.jpg";
+    quizImage.src = "images/01.jpg";
   } else if (text == "好きなゲームは何ですか？") {
     display.textContent =
       "スプラトゥーンです！！！。　トータル2000時間はやっているかも…？";
-    quizImage.src = "images.jpeg";
+    quizImage.src = "images/images.jpeg";
   } else if (text == "何時に寝ますか？") {
     display.textContent = "深夜２時です。　朝は起きれません。";
   } else if (text == "誕生日はいつですか？") {
     display.textContent = "３月３日です。誕生日プレゼント待ってます。　";
-    quizImage.src = "hina.jpg";
+    quizImage.src = "images/hina.jpg";
   } else if (text == "今日は何日ですか？") {
     display.textContent = "今日は2021年５月8日です。";
   } else if (text == "今までの武勇伝は？") {
@@ -35,26 +35,26 @@ serchbutton.onclick = function () {
   } else if (text == "最近観た映画は何ですか？") {
     display.textContent =
       "マネーボール。　プロ野球チームを統計学で勝利に導く映画";
-    quizImage.src = "moneyboll.jpeg";
+    quizImage.src = "images/moneyboll.jpeg";
   } else if (text == "最近買った高い物は何ですか？") {
     display.textContent = "桶太鼓を買いました。　皆さんもmy桶太鼓どうですか？";
   } else if (text == "How are you?") {
     display.textContent = "I'm fine!!!!!!";
-    quizImage.src = "fine.jpeg";
+    quizImage.src = "images/fine.jpeg";
   } else if (text == "好きな和太鼓チームはどこですか？") {
     display.textContent =
       "最近は鼓童にハマっています。　しかし、過酷な環境なので入りたいとは思いません...";
   } else if (text == "好きなポケモンは何ですか？") {
     display.textContent =
       "ヌマクローですかね・・・。　何と言っても、あの無表情かわいいですよね。";
-    quizImage.src = "numa.png";
+    quizImage.src = "images/numa.png";
   } else if (text == "サークル活動してますか？") {
     display.textContent = "大学祭実行委員に入っています!!!";
   } else if (text == "どこに住んでいますか？") {
     display.textContent = "愛知県です！！";
   } else if (text == "名産品はありますか？") {
     display.textContent = "うずらと山芋が入った豊橋カレーうどんが有名ですね";
-    quizImage.src = "karei.jpeg";
+    quizImage.src = "images/karei.jpeg";
   } else if (text == "こんにちは") {
     display.textContent = "hello";
   } else if (text == "有名な太鼓店はどこですか？") {
